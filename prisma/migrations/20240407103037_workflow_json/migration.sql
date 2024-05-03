2b78d3ecd9bfa03e0269be39824853de1be9f9a2
Lei Wu <wulei@microsoft.com>
2024-05-03 15:35:42 +0800

    remove migrations folder

--- prisma/migrations/20240407103037_workflow_json/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `workflow` on the `app` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Json`.
-
-*/
--- AlterTable
-ALTER TABLE `app` MODIFY `workflow` JSON NOT NULL;
